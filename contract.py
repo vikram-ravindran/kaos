a9b502ec91c75a39d9c0f339db80cce28d449002
Vikram Ravindran <v.ravindran@utoronto.ca>
2014-08-25 01:18:01 -0400

    Updated code to run under Python 3.4.1

--- contract.py
+++ contract.py
@@ -1,39 +1,41 @@
 # KAOS contract generator
 # By Vikram Ravindran 
 #
+# See http://en.wikipedia.org/wiki/Assassin_(game) to learn about KAOS
+# 
 # Release history:
+# 2014-08-23  Ported to use Python 3, added explanatory comments
 # 1998        Original release (runs under Python 1.4)
 #
 
-import regex
-import whrandom
+import re
+import random
 
 studentlist=[]
 numbered=[]
 contracts={}
 
-rand=whrandom.whrandom()
 
 # This pattern was created to parse lines of the form:
-# <tr><td><img align=middle src="RS-small.gif"><a href="JDOE.htm">John Doe</a></
-td><td><a href="Boar.htm">Boarder</a></td></tr>
+# <tr><td><img align=middle src="RS-small.gif"><a href="JDOE.htm">John Doe</a></td><td><a href="Boar.htm">Boarder</a></td></tr>
 # from the school's website
 
-namepat='<tr>.*>\([A-Za-z]+ [A-Za-z]+\)<'
-prog=regex.compile(namepat)
+namepat='<tr>.*>([A-Za-z]+ [A-Za-z]+)<'
+prog=re.compile(namepat)
 
 fp=open('Grade12.HTM','r')
 
 while 1:
     line = fp.readline()
     if line=="": break
-    if prog.search(line)!=-1:
-        studentlist.append(prog.group(1))
+    matchObject = prog.search(line)
+    if matchObject!=None:
+        studentlist.append(matchObject.group(1))
 
 fp.close()
 while 1:
     if len(studentlist)==0: break
-    randnum = rand.randint(0,len(studentlist)-1)
+    randnum = random.randint(0,len(studentlist)-1)
     numbered.append(studentlist[randnum])
     del(studentlist[randnum])
 
@@ -43,7 +45,7 @@ for index in range(0,len(numbered)-1):
 
 contracts[numbered[len(numbered)-1]] = numbered[0]
 
-eliminators = contracts.keys()
+eliminators = list(contracts.keys())
 eliminators.sort()
 
 
